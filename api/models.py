4b2364059af75bf6025159dc2dbd0e3e1a625737
Bruno Furtado <brunofurtadoc@gmail.com>
2016-06-17 18:36:11 -0300

    Add category foreign key in product table

--- api/models.py
+++ api/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(default='')
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     category_name = models.CharField(max_length=200)
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    category = models.ForeignKey(to=Category, null=True)
