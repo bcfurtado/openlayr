18f4e437e9d11bfc93180981e3bd8abb157494d2
Bruno Furtado <brunofurtadoc@gmail.com>
2016-06-17 20:16:02 -0300

    Remove uncessary model field from Product

--- api/models.py
+++ api/models.py
@@ -10,5 +10,4 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(default='')
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-    category_name = models.CharField(max_length=200)
-    category = models.ForeignKey(to=Category, null=True)
+    category = models.ForeignKey(to=Category)
